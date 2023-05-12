403838f61ca648ad3d4242ab3ae20659b2271950
matthewmicallef002@gmail.com <matthewmicallef002@gmail.com>
2023-05-12 23:15:09 +0200

    error when connecting app to user api

--- ECommerceApp/Controllers/HomeController.cs
+++ ECommerceApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using ECommerceApp.Models;
+﻿using Common.Models;
+using ECommerceApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace ECommerceApp.Controllers
 {
@@ -22,9 +24,45 @@ namespace ECommerceApp.Controllers
         {
             return View();
         }
-        public IActionResult RegisterUser()
+
+        [HttpGet]
+        public async Task<ActionResult> RegisterUser()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> RegisterUser(User user)
+        {
+            using (var client = new HttpClient())
+            {
+                // Set the URI of the API you want to call
+                client.BaseAddress = new Uri("http://localhost:7097/api/UserMicroservice/RegisterUser"); // replace with your API base address
+
+                // Define request data format  
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+
+                // Sending request to the RegisterUser web api REST service resource using HttpClient  
+                HttpResponseMessage response = await client.PutAsJsonAsync("UserMicroservice/RegisterUser", user);
+
+                // Checking the response is successful or not which is sent using HttpClient  
+                if (response.IsSuccessStatusCode)
+                {
+                    // Do something with the success scenario
+                }
+                else
+                {
+                    // Do something in case of error
+                }
+            }
+
+            // Finally, returning something to the view
+            return View();
+
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
